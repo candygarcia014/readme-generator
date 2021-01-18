80d521141a1224a0810d1799ea054d78863b0a39
Candy Garcia <candygarcia014@gmail.com>
2021-01-17 19:30:41 -0800

    Questions added

--- index.js
+++ index.js
@@ -2,4 +2,30 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // array of questions for user
-const questions = [
+const questions = [[
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Give a description of your project.',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'List any installation instructions.',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'List usage instructions.',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'List any contribution guidelines.',
+        name: 'contributing'
+    },
+];
